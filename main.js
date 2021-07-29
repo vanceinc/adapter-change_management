10d4ae872c22407449f31329977ce4dd8e7f3fbc
vanceinc <david.vance@centurylink.com>
2021-07-29 10:17:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: 'yourDevServiceNowInstanceCanonicalHostname',
-  username: 'devUsername',
-  password: 'devUserPassword'
+  url: 'https://dev82237.service-now.com/',
+  username: 'admin',
+  password: 'ho5p2yuDBVJR'
 };
 
 
